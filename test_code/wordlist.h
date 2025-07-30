a5123bf2c3ef7b7addc19665356ca71bd59a4ff9
Jevith Reddy <jevithgns@gmail.com>
2025-07-31 00:08:41 +0530

    save words

---
+++ test_code/wordlist.h
@@ -0,0 +1,5 @@
+const char *word_list[] = {
+    "THE", "BE", "TO", "OF", "AND", "A", "IN", "THAT", "HAVE", "I", "HELLO", "WORLD"
+    // Add more words...
+};
+const int WORD_LIST_SIZE = sizeof(word_list) / sizeof(word_list[0]);
