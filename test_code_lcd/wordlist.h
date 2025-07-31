e952c12321761d2bd5619074a025c0d2f57182eb
Jevith Reddy <jevithgns@gmail.com>
2025-07-31 14:03:18 +0530

    add wordlist file

---
+++ test_code_lcd/wordlist.h
@@ -0,0 +1,5 @@
+const char *word_list[] = {
+    "THE", "BE", "TO", "OF", "AND", "A", "IN", "THAT", "HAVE", "I", "HELLO", "WORLD"
+    // ... add more words, the more the better
+};
+const int WORD_LIST_SIZE = sizeof(word_list) / sizeof(word_list[0]);
